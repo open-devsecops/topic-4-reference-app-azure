60083fe5aa078c18cfd445035c7e34e0716842a2
Evewwal12 <wxy1012candoit@163.com>
2025-05-26 22:38:02 -0700

    update the monthly zone revenue to be a table rather than a view

--- sql/create_monthly_zone_revenue.sql
+++ sql/create_monthly_zone_revenue.sql
@@ -1,17 +1,23 @@
-CREATE VIEW dbo.vw_monthly_zone_revenue AS
+CREATE TABLE dbo.monthly_zone_revenue (
+  trip_month NVARCHAR(7),      
+  pickup_zone NVARCHAR(255),
+  taxi_type NVARCHAR(10),
+  trip_count INT,
+  total_revenue FLOAT
+);
+GO
+
+INSERT INTO dbo.monthly_zone_revenue (trip_month, pickup_zone, taxi_type, trip_count, total_revenue)
 SELECT
- FORMAT(pickup_datetime, 'yyyy-MM') AS trip_month,   -- eg. 2025-01
- pickup_zone,                                        
- taxi_type,                                          -- yellow / green
- COUNT(*) AS trip_count,                             
- SUM(total_amount) AS total_revenue                  
-FROM dbo.fact_trip                                    
+  FORMAT(pickup_datetime, 'yyyy-MM') AS trip_month,
+  pickup_zone,
+  taxi_type,
+  COUNT(*) AS trip_count,
+  SUM(total_amount) AS total_revenue
+FROM dbo.fact_trip
 WHERE pickup_datetime IS NOT NULL
 GROUP BY
- FORMAT(pickup_datetime, 'yyyy-MM'),
- pickup_zone,
- taxi_type;
-
-----------------------------------
-select * from dbo.vw_monthly_zone_revenue
-order by trip_month, pickup_zone
+  FORMAT(pickup_datetime, 'yyyy-MM'),
+  pickup_zone,
+  taxi_type;
+GO
